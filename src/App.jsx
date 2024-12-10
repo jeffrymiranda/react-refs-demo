dc6d32a65ada38d73447ac001eec2f7a45c5c64a
Jeff <jeffryuna@gmail.com>
2024-12-10 13:55:09 -0600

    Collecting User Input with Refs & Forwarded Refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
-import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import {useState} from "react";
 import NewProject from "./components/NewProject.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
 
@@ -19,9 +19,25 @@ function App() {
         });
     }
 
+    function handleAddProject(projectData) {
+        setProjectsState(prevState => {
+            const newProject = {
+                ...projectData,
+                id: Math.random()
+            };
+
+            return {
+                ...prevState,
+                projects: [...prevState.projects, newProject]
+            }
+        });
+    }
+
+    console.log(projectsState);
+
     let content;
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject/>;
+        content = <NewProject onAdd={handleAddProject}/>;
     } else if (projectsState.selectedProjectId === undefined) {
         content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
     }
