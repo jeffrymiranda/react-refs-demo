ce7aa1749dd2ba70226a9bafbe90b11928f5919d
Jeff <jeffryuna@gmail.com>
2024-12-11 15:01:13 -0600

    Handling Project Deletion

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,17 @@ function App() {
     });
 
     function handleStartAddProject() {
-        setProjectsState(prevState => {
-            return {
-                ...prevState,
-                selectedProjectId: null
-            }
-        });
+        setProjectsState(prevState => ({
+            ...prevState,
+            selectedProjectId: null
+        }));
     }
 
     function handleCancelAddProject() {
-        setProjectsState(prevState => {
-            return {
-                ...prevState,
-                selectedProjectId: undefined
-            }
-        });
+        setProjectsState(prevState => ({
+            ...prevState,
+            selectedProjectId: undefined
+        }));
     }
 
     function handleAddProject(projectData) {
@@ -46,17 +42,22 @@ function App() {
     }
 
     function handleSelectProject(id) {
-        setProjectsState(prevState => {
-            return {
-                ...prevState,
-                selectedProjectId: id
-            }
-        });
+        setProjectsState(prevState => ({
+            ...prevState,
+            selectedProjectId: id
+        }));
     }
 
-    const selectedProject = projectsState.projects
-        .find(project => project.id === projectsState.selectedProjectId);
-    let content = <SelectedProject project={selectedProject}/>;
+    function handleDeleteProject() {
+        setProjectsState(prevState => ({
+            ...prevState,
+            selectedProjectId: undefined,
+            projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
+        }));
+    }
+
+    const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
     if (projectsState.selectedProjectId === null) {
         content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
